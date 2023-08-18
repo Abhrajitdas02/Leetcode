22b10339f4de96cecfd86c8edc39ab5648125a45
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2023-08-19 01:17:03 +0530

    Time: 60 ms (5.33%), Space: 44.5 MB (10.15%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,31 @@
 class Solution {
-    private void findpermute(List<Integer>ds,boolean[] arr, int[] nums, List<List<Integer>>ans){
-        if(ds.size()==nums.length){
-            ans.add(new ArrayList<>(ds));
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        finPermu(nums, 0, res);
+        return res;
+    }
+    private void finPermu(int nums[], int startIndx, List<List<Integer>> res){
+        int sz = nums.length;
+        for(var num : nums){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        if(startIndx == sz){
+            List<Integer> currPer = new ArrayList<>();
+            for(var num : nums)currPer.add(num);
+            res.add(currPer);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!arr[i]){
-                arr[i]=true;
-                ds.add(nums[i]);
-                findpermute(ds,arr,nums,ans);
-                ds.remove(ds.size()-1);
-                arr[i]=false;
-            }
+        for(int leftPtr = startIndx ; leftPtr < sz; leftPtr++){
+                swap(nums, startIndx, leftPtr);
+                finPermu(nums, startIndx+1, res);
+                swap(nums, startIndx, leftPtr);
         }
     }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ds=new ArrayList<>();
-        boolean arr[]=new boolean[nums.length];
-        findpermute(ds,arr,nums,ans);
-        return ans;
+    private void swap(int nums[], int indx1, int indx2){
+        int tempNum = nums[indx1];
+        nums[indx1] = nums[indx2];
+        nums[indx2] = tempNum;
     }
+
 }
