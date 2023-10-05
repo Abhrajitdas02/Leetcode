4e25606410a658ae0a66046f3331deaa0267dc16
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2023-10-06 03:00:31 +0530

    Time: 5 ms (43.30%), Space: 9.4 MB (12.84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,32 @@
+// class Solution {
+//     private:
+//     int find(int n,int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+//         if(i==n-1) return triangle[n-1][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int down=triangle[i][j]+find(n,i+1,j,triangle,dp);
+//         int diag=triangle[i][j]+find(n,i+1,j+1,triangle,dp);
+//         return dp[i][j]=min(down,diag);
+//     }
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n=triangle.size();
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         return find(n,0,0,triangle,dp);
+//     }
+// };
 class Solution {
-    private:
-    int find(int n,int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        if(i==n-1) return triangle[n-1][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down=triangle[i][j]+find(n,i+1,j,triangle,dp);
-        int diag=triangle[i][j]+find(n,i+1,j+1,triangle,dp);
-        return dp[i][j]=min(down,diag);
-    }
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return find(n,0,0,triangle,dp);
-    }
+    public:
+     int minimumTotal(vector<vector<int>>& triangle) {
+       int n=triangle.size();
+         vector<vector<int>> dp(n,vector<int>(n,0));
+         for(int j=0;j<n;j++) dp[n-1][j]=triangle[n-1][j]; //base case
+          for(int i=n-2;i>=0;i--){
+              for(int j=i;j>=0;j--){
+                 int down=triangle[i][j]+dp[i+1][j];
+                 int diag=triangle[i][j]+dp[i+1][j+1];
+                 dp[i][j]=min(down,diag);
+             }
+         }
+         return dp[0][0];
+     }
 };
