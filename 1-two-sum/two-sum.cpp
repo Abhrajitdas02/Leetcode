2f9d58e2c7862c48c5e62a86e7672f79c4ef3427
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-05-23 01:18:24 +0530

    Time: 3 ms (98.56%) | Memory: 13.7 MB (41.22%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n =nums.size();
+//         unordered_map<int,int> mp;
+//         for(int i=0;i<n;i++){
+//             int b = target-nums[i];
+//            if(mp.find(b) != mp.end()){
+//             return {mp[b],i};
+//            }
+//             mp[nums[i]]=i;
+//          }
+//             return {-1,-1};
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n =nums.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            int b = target-nums[i];
-           if(mp.find(b) != mp.end()){
-            return {mp[b],i};
-           }
-            mp[nums[i]]=i;
-         }
-            return {-1,-1};
+
+        vector<pair<int,int> > temp;
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i++){
+            pair <int,int> pr = {nums[i], i};
+            temp.push_back(pr);
+        }   
+
+        sort (temp.begin(), temp.end());
+
+        int s = 0;
+        int e = nums.size()-1;
+
+        while(s < e){
+            int sum = temp[s].first + temp[e].first;
+            if(sum == target){
+                ans.push_back(temp[s].second);
+                ans.push_back(temp[e].second);
+                break;
+            } 
+            if(sum < target){
+                s++;
+            }
+            else {
+                e--;
+            }
+        }
+        return ans;
     }
 };
