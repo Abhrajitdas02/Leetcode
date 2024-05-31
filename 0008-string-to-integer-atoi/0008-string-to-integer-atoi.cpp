aab49edaabee77fd5e0db7cc1fef4737e364b8c5
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-05-31 14:47:26 +0530

    Time: 3 ms (57.71%), Space: 8.3 MB (37.33%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,45 @@
 class Solution {
- public:
-  int myAtoi(string s) {
-    trim(s);
-    if (s.empty())
-      return 0;
-
-    const int sign = s[0] == '-' ? -1 : 1;
-    if (s[0] == '+' || s[0] == '-')
-      s = s.substr(1);
-
-    long num = 0;
-
-    for (const char c : s) {
-      if (!isdigit(c))
-        break;
-      num = num * 10 + (c - '0');
-      if (sign * num < INT_MIN)
-        return INT_MIN;
-      if (sign * num > INT_MAX)
-        return INT_MAX;
+public:
+    int myAtoi(string s) {
+        int n=s.size();
+        int i=0;
+        long long num=0;
+        int flag=1;
+        while(i<n){
+            while(s[i]==' ' && i<n) i++;
+            if(i==n) break;
+            if(isdigit(s[i])){
+                while(isdigit(s[i])){
+                  num=num*10 + (s[i]-'0');
+                  if(num>=INT_MAX) return INT_MAX;
+                    i++;
+                }
+                break;
+            }
+            else if(s[i]=='+' ){
+                flag=1;
+                i++;
+                while(isdigit(s[i])){
+                    num=num*10 + (s[i]-'0');
+                    if(num>=INT_MAX) return INT_MAX;
+                    i++;
+                }
+                break;
+            }
+            else if(s[i]=='-'){
+                flag=-1;
+                i++;
+                while(isdigit(s[i])){
+                     num=num*10 + (s[i]-'0');
+                     if(flag*num<=INT_MIN) return INT_MIN;
+                    i++;
+                }
+                break;
+            }
+            else  break;
+        }
+        
+        num=num*flag;
+        return num;
     }
-
-    return sign * num;
-  }
-
- private:
-  void trim(string& s) {
-    s.erase(0, s.find_first_not_of(' '));
-    s.erase(s.find_last_not_of(' ') + 1);
-  }
 };
