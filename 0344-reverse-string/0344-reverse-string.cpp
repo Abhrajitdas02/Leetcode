f7a6f141d5ca363361c8a93b737f69556c4ee3c7
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-06-01 14:16:44 +0530

    Time: 16 ms (68.44%), Space: 26.8 MB (99.66%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,11 +1,15 @@
 class Solution {
+private:
+    void find(int i,int j,vector<char>& s){
+           if(i>=j) return;
+           else{
+            swap(s[i],s[j]);
+            return find(i+1,j-1,s);
+           }
+    }
 public:
     void reverseString(vector<char>& s) {
         int l=s.size();
-        for(int i=l-1;i>=l/2;i--){
-           char t=s[i];
-            s[i]=s[l-1-i];
-            s[l-1-i]=t;
-      }
+        return find(0,l-1,s);
     }
 };
