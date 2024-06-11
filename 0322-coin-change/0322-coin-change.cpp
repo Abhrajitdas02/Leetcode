e428ed76c6b534c113b58c2b95a725be3005296d
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-06-11 20:56:02 +0530

    Time: 35 ms (52.53%), Space: 46.9 MB (29.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,25 @@
 class Solution {
-    private:
-    int f(int ind,int amount,vector<int>coins,vector<vector<int>> &dp){
-        if(ind == 0){
-            if(amount % coins[0] == 0) return amount/ coins[0];
-            return 1e9;
-        }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        int notTake = 0 + f(ind-1, amount,coins,dp);
-        int take= INT_MAX;
-        if(coins[ind] <=amount) take = 1+ f(ind,amount-coins[ind], coins,dp);
-        return dp[ind][amount] = min(take,notTake);
+private:
+   int find(int ind, vector<int>& coins, int amt,vector<vector<int>>& dp){
+    if(ind==0){
+        if(amt%coins[ind]==0) return amt/coins[ind];
+        else return 1e9;
     }
-    
+    if(dp[ind][amt]!=-1) return dp[ind][amt];
+
+    int nopick=find(ind-1,coins,amt,dp);
+    int pick=INT_MAX;
+    if(coins[ind]<=amt) pick=1+ find(ind,coins,amt-coins[ind],dp);
+
+    return dp[ind][amt]= min(pick,nopick);
+   }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount +1, -1));
-        int ans= f(n-1,amount,coins,dp);
-        if(ans >= 1e9) return -1;
+        int n = coins.size();
+        sort(coins.begin(),coins.end());
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans=find(n-1,coins,amount,dp);
+        if(ans>=1e9) return -1;
         return ans;
     }
 };
