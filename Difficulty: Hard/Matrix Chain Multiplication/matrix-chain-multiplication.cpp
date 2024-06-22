8d11a1e11ba7e050ac6318c65d96fbc6e009f0ab
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-06-23 03:09:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -8,23 +8,44 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-private:
-   int find(int i,int j, int arr[],vector<vector<int>>& dp){
-       if(i==j) return 0;
-       if(dp[i][j]!=-1) return dp[i][j];
-       int mini=1e9;
-       for(int k=i;k<j;k++){
-           int steps= arr[i-1]*arr[k]*arr[j] + find(i,k,arr,dp)+find(k+1,j,arr,dp);
-           if(steps<mini) mini=steps;
-       }
-       return dp[i][j]=mini;
-   }
+// private:
+//   int find(int i,int j, int arr[],vector<vector<int>>& dp){
+//       if(i==j) return 0;
+//       if(dp[i][j]!=-1) return dp[i][j];
+//       int mini=1e9;
+//       for(int k=i;k<j;k++){
+//           int steps= arr[i-1]*arr[k]*arr[j] + find(i,k,arr,dp)+find(k+1,j,arr,dp);
+//           if(steps<mini) mini=steps;
+//       }
+//       return dp[i][j]=mini;
+//   }
 public:
     int matrixMultiplication(int N, int arr[])
     {
-        vector<vector<int>>dp(N,vector<int>(N,-1));
-        return find(1,N-1,arr,dp);
         // code here
+        int dp[N][N];
+        
+        for(int i=1;i<N;i++){
+            dp[i][i] = 0;
+        }
+        
+        for(int i = N - 1; i >= 1; i--){
+            for(int j = i + 1; j < N; j++){
+                
+                int mini = 1e9;
+                for(int k = i; k < j; k++){
+                    int steps = arr[i-1] * arr[k] * arr[j] 
+                    + dp[i][k]
+                    + dp[k+1][j];
+                    
+                    if(steps < mini) mini = steps;
+                }
+                dp[i][j] = mini;
+                
+            }
+        }
+        
+        return dp[1][N-1];
     }
 };
 
