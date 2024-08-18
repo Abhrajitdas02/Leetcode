e807b008768bf5169a425171d58ff3ce15086e90
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-08-19 02:59:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.52%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.cpp
+++ 2235-capitalize-the-title/2235-capitalize-the-title.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     string capitalizeTitle(string title) {
         transform(title.begin(), title.end(), title.begin(), ::tolower);
-        string ans = "";
-        string word = "";
         
-        for (int i = 0; i < title.size(); ++i) {
-            if (title[i] != ' ') {
-                word.push_back(title[i]);
-            } else {
-                if (word.size() > 2) {
-                    word[0] = toupper(word[0]);
-                }
-                ans += word + " ";
-                word = "";
+        int n=title.size();
+        int start = 0;
+        int end = title.size();
+        int size;
+        int i=0;
+
+        while (i < n) {
+
+            start = i;
+            while (i < n && title[i] != ' ') {
+
+                i++;
             }
+            end = i - 1;
+            size = end - start + 1;
+            if (size > 2) {
+                title[start] = toupper(title[start]);
+            }
+            i++;
         }
-        
-        if (word.size() > 2) {
-            word[0] = toupper(word[0]);
-        }
-        ans += word; 
-        
-        return ans;
+        return title;
     }
 };
