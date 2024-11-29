d038b31c664be09bdd63aee197eb8a116abdc17e
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-11-30 01:34:05 +0530

    Time: 4 ms (22.94%), Space: 8.4 MB (10.46%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -4,19 +4,19 @@ public:
         int n=word1.length();
         int m=word2.length();
         string ans="";
-        if(m>=n){
-            for(int i=0;i<n;i++){
-                ans+=word1[i];
-                ans+=word2[i];
-            }
-            for(int i=n;i<m;i++) ans+=word2[i];
-        }
-        else{
+        if(n>m){
             for(int i=0;i<m;i++){
                 ans+=word1[i];
                 ans+=word2[i];
             }
             for(int i=m;i<n;i++) ans+=word1[i];
+        }
+        else{
+            for(int i=0;i<n;i++){
+                ans+=word1[i];
+                ans+=word2[i];
+            }
+            for(int i=n;i<m;i++) ans+=word2[i];
         }
         return ans;
     }
