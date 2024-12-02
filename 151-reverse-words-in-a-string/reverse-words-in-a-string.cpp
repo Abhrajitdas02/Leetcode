db458187b5474ca570bc96561486a3cc3c48e78d
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-12-02 13:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (27.25%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string>v;
-        string temp="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=' '){
-                temp+=s[i];
-            }
-            else{
-                if(!temp.empty()){
-                    v.push_back(temp);
-                    temp="";
-                }
-            }
+        istringstream stream(s);
+        string word;
+        vector<string> words;
+        string ans="";
+        while(stream >> word){
+            words.push_back(word);
         }
-        if(!temp.empty()) v.push_back(temp);
-        string res="";
-        for(int i=v.size()-1;i>=0;i--){
-            res+=v[i];
-            if(i>0)
-            res+=" ";
+        for(int i=words.size()-1;i>=0;i--){
+            if(i==0) 
+            ans+=words[i];
+            else
+            ans+=words[i]+' ';
         }
-        return res;
+        return ans;
     }
 };
