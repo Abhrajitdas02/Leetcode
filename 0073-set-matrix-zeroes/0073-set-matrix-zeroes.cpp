f1515c77318b9bed8ef058b12fc868d6deff037c
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-12-18 14:28:38 +0530

    Time: 0 ms (100%), Space: 18.7 MB (17.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int,int>x;
-        unordered_map<int,int>y;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<pair<int,int>> pos;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    x[i]=1;
-                    y[j]=1;
+                    pos.push_back({i,j});
                 }
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(x[i] || y[j]){
-                    matrix[i][j]=0;
-                }
+        for(auto i:pos){
+            for(int x=0;x<n;x++){
+                matrix[i.first][x]=0;
+            }
+            for(int x=0;x<m;x++){
+                matrix[x][i.second]=0;
             }
         }
-
-
-        
-        
     }
 };
