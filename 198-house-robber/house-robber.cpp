25759cfb5ba578fe26f00cb3ec5d71d0032acde3
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-12-26 19:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (11.65%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int find(int ind,vector<int>& nums,vector<int>& dp){
-        if(ind==0)return nums[ind];
-        if(ind<0)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        int pick,nopick;
-        pick=nums[ind]+find(ind-2,nums,dp);
-        nopick=0+find(ind-1,nums,dp);
-        return dp[ind]=max(pick,nopick);
-    }
+    // int find(int ind,vector<int>& nums,vector<int>& dp){
+    //     if(ind==0)return nums[ind];
+    //     if(ind<0)return 0;
+    //     if(dp[ind]!=-1)return dp[ind];
+    //     int pick=nums[ind]+find(ind-2,nums,dp);
+    //     int nopick=0+find(ind-1,nums,dp);
+    //     return dp[ind]=max(pick,nopick);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if(n==1)return nums[0];
         vector<int> dp(n,-1);
-        return find(n-1,nums,dp);
+        dp[0]=nums[0];
+        int pick;
+        for(int i=1;i<n;i++){
+            pick=nums[i]; if(i>1)pick+=dp[i-2];
+            int nopick=0+dp[i-1];
+            dp[i]=max(pick,nopick);
+        }
+        return dp[n-1];
     }
 };
