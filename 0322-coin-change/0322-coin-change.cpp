f258cd98b1061a9d250098a904736493bdec9160
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2024-12-30 23:34:15 +0530

    Time: 41 ms (41.38%), Space: 49 MB (24.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,15 @@
 class Solution {
 private:
    int find(int ind, vector<int>& coins, int amt,vector<vector<int>>& dp){
-    if(ind==0){
-        if(amt%coins[ind]==0) return amt/coins[ind];
-        else return 1e9;
+   if(ind==0){
+    if(amt%coins[ind]==0)return amt/coins[0];
+    else return 1e9;
     }
-    if(dp[ind][amt]!=-1) return dp[ind][amt];
-
+    if(dp[ind][amt]!=-1)return dp[ind][amt];
     int nopick=find(ind-1,coins,amt,dp);
     int pick=INT_MAX;
-    if(coins[ind]<=amt) pick=1+ find(ind,coins,amt-coins[ind],dp);
-
-    return dp[ind][amt]= min(pick,nopick);
+    if(coins[ind]<=amt)pick=1+find(ind,coins,amt-coins[ind],dp);
+    return dp[ind][amt]=min(pick,nopick);
    }
 public:
     int coinChange(vector<int>& coins, int amount) {
