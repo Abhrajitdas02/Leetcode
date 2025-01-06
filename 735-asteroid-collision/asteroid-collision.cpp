6363033ef441389918c24e8a0d42fb1d0ca84ccf
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-01-06 14:51:48 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (25.62%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -6,12 +6,12 @@ public:
         for(int i=0;i<n;i++){
             if(asteroids[i]>0)st.push(asteroids[i]);
             else{
-            while(!st.empty() && st.top()>0 && abs(asteroids[i])>st.top())
-            st.pop();
-            if(!st.empty() && st.top()==abs(asteroids[i]))
-            st.pop();
-            else if(st.empty() || st.top()<0)
-            st.push(asteroids[i]);
+                while(!st.empty() && st.top()>0 && abs(asteroids[i])>st.top())
+                st.pop();
+                if(!st.empty() && st.top()==abs(asteroids[i]))
+                st.pop();
+                else if(st.empty() || st.top()<0)
+                st.push(asteroids[i]);
             }
         }
         vector<int> ans;
