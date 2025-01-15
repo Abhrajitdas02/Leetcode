cf8e0742a00ff9056d2fe2c7717c8a5a680b9beb
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-01-15 17:07:50 +0530

    Time: 43 ms (47.27%), Space: 75.5 MB (41.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,28 +1,55 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    ListNode* mergeLL(ListNode*list1,ListNode*list2){
+        ListNode*dummynode = new ListNode(-1);
+        ListNode*temp = dummynode;
+
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val < list2->val){
+                temp->next = list1;
+                temp = list1;
+                list1=list1->next;
+            }
+            else{
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            }
+        }
+        if(list1){
+            temp->next = list1;
+        }
+        else{
+            temp->next = list2;
+        }
+
+        return dummynode->next;
+    }
+    ListNode* findmid(ListNode*head){
+        ListNode*fast = head->next;
+        ListNode*slow = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    ListNode* sortLL(ListNode*head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode*middle = findmid(head);
+        ListNode*left = head;
+        ListNode*right = middle->next;
+        middle->next = NULL;
+
+        left = sortLL(left);
+        right = sortLL(right);
+
+        return mergeLL(left,right);
+    }
     ListNode* sortList(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> ans;
-        while(temp){
-            ans.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(ans.begin(),ans.end());
-        temp=head;
-        for(int i=0;i<ans.size();i++){
-            temp->val=ans[i];
-            temp=temp->next;
-        }
-        return head;
+        return sortLL(head);
     }
 };
