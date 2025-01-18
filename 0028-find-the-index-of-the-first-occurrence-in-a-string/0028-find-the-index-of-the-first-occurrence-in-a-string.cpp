ef03c987ad99d0fd7e912508fc7f01e078c08cd3
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-01-18 12:52:35 +0530

    Time: 0 ms (100%), Space: 9 MB (12.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,12 +1,50 @@
-class Solution {
+class Solution {  //KMP
 public:
-    int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        for(int i=0;i<=(n-m);i++){
-            if(haystack.substr(i,m)==needle)
-            return i;
+void lpsfind(vector<int>&lps,string s){
+    int pre=0,suf=1;
+    while(suf<s.size()){
+        if(s[pre]==s[suf])
+        {
+            lps[suf]=pre+1;
+            suf++,pre++;
         }
+        else
+        {
+            if(pre==0)
+            {
+                lps[suf]=0;
+                suf++;
+            }
+            else
+            {
+                pre=lps[pre-1];
+            }
+        }
+    }
+}
+    int strStr(string haystack, string needle) {
+        vector<int>lps(needle.size(),0);
+        lpsfind(lps,needle);
+        int first=0 , second=0;
+        while(first<haystack.size()&& second<needle.size())
+        {
+            // Match
+            if(haystack[first]==needle[second])
+            {
+                first++,second++;
+            }
+            //Not Match 
+            else
+            {
+                if(second==0)  // case
+                first++;
+                else
+                second=lps[second-1];
+            }
+        }
+        // anser exist
+        if(second==needle.size())
+        return first-second;
         return -1;
     }
 };
