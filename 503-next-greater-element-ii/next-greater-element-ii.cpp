4274fe0bc4b1484eb37e6f030b6bd9251e60243e
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-01-25 13:50:51 +0530

    Time: 4 ms (61.51%) | Memory: 28.4 MB (80.52%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=2*n-1;i>=0;i--){
             while(!st.empty() && st.top()<=nums[i%n])
             st.pop();
-            if(i<n){
-            if(st.empty())ans[i]=-1;
-            else ans[i]=st.top();
-            }
-            st.push(nums[i%n]);
+        if(i<n){
+            if(st.empty())ans[i%n]=-1;
+            else ans[i%n]=st.top();
+        }
+        st.push(nums[i%n]);
         }
         return ans;
     }
