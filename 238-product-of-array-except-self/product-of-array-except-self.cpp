7f698b63efa24b0cd52f7c07e5f54f6e1b133a73
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-01-27 01:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (79.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         vector<int> ans(n,1);
 
-        for(int i=1;i<n;i++){
-            ans[i]=nums[i-1]*ans[i-1];
-        }
+        for(int i=1;i<n;i++)
+        ans[i]=nums[i-1]*ans[i-1];
+
         int suffix=1;
         for(int i=n-1;i>=0;i--){
             ans[i]*=suffix;
