e7ceff8823f7f0ec69e820c275293c3e88db439d
Abhrajit Das <91903920+Abhrajitdas02@users.noreply.github.com>
2025-02-03 12:47:16 +0530

    Time: 19 ms (31.54%), Space: 14.3 MB (30.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,15 @@ public:
         int n=s.length();
         unordered_set<char> st;
         int left=0;
-        int maxLen=0;
+        int maxlen=0;
         for(int right=0;right<n;right++){
             while(st.find(s[right])!=st.end()){
-            st.erase(s[left]);
-            left++;
+                st.erase(s[left]);
+                left++;
             }
-        st.insert(s[right]);
-        maxLen=max(maxLen,right-left+1);
+            st.insert(s[right]);
+            maxlen=max(maxlen,right-left+1);
         }
-        return maxLen;
+        return maxlen;
     }
 };
